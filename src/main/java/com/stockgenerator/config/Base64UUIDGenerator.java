ed08247b42e2651866c7ccb54abe7bc6edb9382e
Dima Ivaniuta <divanyuta@adigida.com>
2025-02-22 13:25:09 +0100

    Base64UUIDGenerator util class

--- src/main/java/com/stockgenerator/config/Base64UUIDGenerator.java
+++ src/main/java/com/stockgenerator/config/Base64UUIDGenerator.java
@@ -1,2 +1,25 @@
-package com.stockgenerator.config;public class Base64UUIDGenerator {
+package com.stockgenerator.config;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.ByteBuffer;
+import java.util.Base64;
+import java.util.UUID;
+
+@Slf4j
+public class Base64UUIDGenerator {
+
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        String base64UUID = encodeBase64UUID(uuid);
+        log.info("Base64 UUID: {}", base64UUID);
+    }
+
+    public static String encodeBase64UUID(UUID uuid) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[16]);
+        byteBuffer.putLong(uuid.getMostSignificantBits());
+        byteBuffer.putLong(uuid.getLeastSignificantBits());
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(byteBuffer.array());
+    }
+
 }
